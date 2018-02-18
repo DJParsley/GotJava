063d3c626e23ca2ee207490658d2c2f6678e010c
M-Sloan <murraysloan90@gmail.com>
2018-02-18 15:28:59 -0500

    Initial commit of Java source code

---
+++ src/GotJava.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Murray
+ *
+ */
+public class GotJava {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
